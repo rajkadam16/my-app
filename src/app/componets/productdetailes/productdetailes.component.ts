460e416f0da0878803628a617c66ec2fba59384b
rajkadam16 <152425457+rajkadam16@users.noreply.github.com>
2024-06-22 14:00:59 +0530

    dev(fix):add multiple componets

---
+++ src/app/componets/productdetailes/productdetailes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productdetailes',
+  templateUrl: './productdetailes.component.html',
+  styleUrls: ['./productdetailes.component.css']
+})
+export class ProductdetailesComponent {
+
+}
